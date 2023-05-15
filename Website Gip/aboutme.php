93559c9f0898b6ee8f7a5e63bd854a8875ab12b8
Xander <xanderdepaepe@gmail.com>
2023-05-16 01:34:56 +0200

    Add files via upload

---
+++ Website Gip/aboutme.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>XANN gip site</title>
+    <!-- Bootstrap -->
+    <link href="css/bootstrap-4.4.1.css" rel="stylesheet">
+	  <link href="css/style.css" rel="stylesheet">
+  </head>
+	<?php include_once "navbar.php"; ?>
+  <body>
+  
+    <section>
+      <div class="jumbotron text-center mt-2">
+      <section class="intro">
+    <div class="column">
+      <img src="img/profilepic.jpg" target="_blank" alt="" class="profile" >
+	  </a>
+    </div>
+	  
+    <div class="column">
+      
+      <p>Welkom op mijn vlog-pagina voor het vastleggen van stage lopen in Italië! Hier vind je een verzameling vlogs die mijn ervaringen met leven, werken en het verkennen van het prachtige land Italië documenteren. Ons team van stagiaires heeft vanuit Vlaanderen gereisd om te leren en zich onder te dompelen in de Italiaanse cultuur. Via mijn vlogs hoop ik de schoonheid van de Italiaanse taal, keuken, kunst en architectuur met je te delen. Of het nu gaat om de drukke straten van Cesenatico of de prachtige strande , we hebben het allemaal in onze vlogs. Dus leun achterover, ontspan en ga met ons mee op deze spannende reis door Italië!</p>
+<p>&nbsp;</p>
+
+    </div>
+  </section>
+      </div>
+</section>
+  
+<section>
+      <div class="container">
+        <div class="row">
+          <div class="col-12 text-center">
+            <h2>Door leerkracht gekozen</h2>
+            <p>Uiteindelijk heeft mnr. Lippens gekozen voor de Chatapp.</p>
+            
+          </div>
+        </div>
+        
+    </section>
+
+    
+    <footer class="text-center">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <p>Copyright © Xander De Paepe</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <!-- jQuery (javascript bootstrap) -->
+  <script src="js/jquery-3.4.1.min.js"></script>
+    <!-- plugins -->
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap-4.4.1.js"></script>
+  </body>
+</html>
