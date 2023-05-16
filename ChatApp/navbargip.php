4dafa0c314b03f880fa5f4ec01b994b37c45ed80
Xander <xanderdepaepe@gmail.com>
2023-05-16 09:48:54 +0200

    Add files via upload

---
+++ ChatApp/navbargip.php
@@ -0,0 +1,34 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a class="navbar-brand" href="#"><img src="img/XANN LOGO.png" class="navimg" alt="XANN"></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="../index.php">Home <span class="sr-only"></span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../aboutme.php">About me</a>
+          </li>
+			<li class="nav-item">
+            <a class="nav-link" href="index.php">GIP</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Meer&nbsp; </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              
+				<a class="dropdown-item" href="../vlogs.html">Vlogs</a>
+				<a class="dropdown-item" href="vlogs.html">Vlogs</a>
+				<a class="dropdown-item" href="vlogs.html">Vlogs</a>
+				<a class="dropdown-item" href="vlogs.html">Vlogs</a>
+				<div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="vlogs.html">Stage verslag</a>
+				<a class="dropdown-item" href="vlogs.html">Vlogs</a>
+				
+            </div>
+          </li>
+        </ul>
+	</div>
+</nav>
